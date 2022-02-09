3e5a79d767e4e7d9e977df5f078e7bb4491d8001
MarkReddy87 <markreddy87@gmail.com>
2022-02-09 13:00:33 +0000

    Removed empty white space and added necessary blank lines

--- bag/views.py
+++ bag/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -19,5 +21,5 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
-    request.session['bag'] = bag 
+    request.session['bag'] = bag
     return redirect(redirect_url)
