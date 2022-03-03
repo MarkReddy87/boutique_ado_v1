e202fb8e96207a03baf687da407040becafd5754
MarkReddy87 <markreddy87@gmail.com>
2022-03-03 13:22:16 +0000

    recommented out database heading in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -119,8 +119,8 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
